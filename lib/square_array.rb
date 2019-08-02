d6ede235ede3c8c1f27564d4a6e375dd23ba1694
Katherine Kelly <katherinewkwong@gmail.com>
2019-08-02 00:17:07 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
-  # your code here
+  i = 0
+  squared_arr = []
+  
+  while i < array.length do
+    squared_arr << array[i] ** 2
+    i += 1
+  end
+  squared_arr
 end
+
+# def square_array(array)
+#   array.map { |num| << num ** 2 }
+# end
